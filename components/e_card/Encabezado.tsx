cd8301bed91b97c24bd4a76842845517a4b3a05a
Ali_trk <trk.0010.0011@gmail.com>
2026-06-13 09:26:54 -0400

    componente Encabezado realizado

---
+++ components/e_card/Encabezado.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '@/components/e_card/Encabezado.css'
+
+export default function Encabezado() {
+  return (
+    <div className="line-encabezado">
+        <span>Exercícios</span>
+        <span>4</span>
+    </div>
+  )
+}
